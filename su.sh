18c2ee0bcff715432dba20157b142e2ebad90303
Victor Schwan <victor.schwan@cern.ch>
2024-09-20 14:48:14 +0900

    basic setup script su.sh added

---
+++ su.sh
@@ -0,0 +1,15 @@
+# Function to print messages in specified color
+print_color() {
+    # ANSI yellow
+    local YELLOW='\033[1;33m'
+    # ANSI no color (reset)
+    local NC='\033[0m'
+    local PREFIX="[ setup ]:"
+    echo
+    echo -e "${YELLOW}${PREFIX}${NC} $1"
+    echo
+}
+
+print_color "set local k4geo"
+export k4geo_DIR="$HOME/promotion/code/k4geo"
+export K4GEO="$HOME/promotion/code/k4geo"
