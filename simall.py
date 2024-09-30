3e3e97825759faa1671d469a7b6c038e4980a7ca
Victor Schwan <victor.schwan@cern.ch>
2024-09-30 17:13:30 +0900

    simall.py: new detector models, check max bx number exceeded

--- simall.py
+++ simall.py
@@ -10,22 +10,25 @@ class DetectorConfig:
 
 
 # Define the variables
+executeBsub = False  # Boolean variable to switch between modes
+
 bunchCrossingEnd = 2
 nEvents = 5000
 guineaPigPartPerE = -1
 versionName = "python_test"
 detMods2Ana = {"ILD_FCCee_v01", "ILD_FCCee_v02"}
 acceleratorConfigs2Ana = {"FCC91", "FCC240"}
-executeBsub = False  # Boolean variable to switch between modes
 
 
 outDir = Path.home() / "promotion" / "data" / versionName
 # define dirs to detector models
 k4geoDir = Path.home() / "promotion" / "code" / "k4geo"
 ild4FCCDir = Path("FCCee") / "ILD_FCCee" / "compact"
+ild4ILCDir = Path("ILD") / "compact" / "ILD_sl5_v02"
 
 # define Paths to ddsim files
-ddsim4FCCee = Path.home() / "promotion/code/bs/ddsim_keep_microcurlers_10MeV_30mrad.py"
+ddsim4FCC = Path.home() / "promotion/code/bs/ddsim_keep_microcurlers_10MeV_30mrad.py"
+ddsim4ILC = Path.home() / "promotion/code/bs/ddsim_keep_microcurlers_10MeV.py"
 
 # Source the setup script (this will be a no-op in Python, since sourcing doesn't propagate in subprocess)
 setupScriptPath = "/cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh"
@@ -33,13 +36,21 @@ setupScriptPath = "/cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh"
 # Dict containing the detector model configurations
 detectorConfigs = {
     "ILD_FCCee_v01": DetectorConfig(
-        ddsim4FCCee, ild4FCCDir / "ILD_FCCee_v01/ILD_FCCee_v01.xml"
+        ddsim4FCC, ild4FCCDir / "ILD_FCCee_v01/ILD_FCCee_v01.xml"
+    ),
+    "ILD_FCCee_v01_fields": DetectorConfig(
+        ddsim4FCC, ild4FCCDir / "ILD_FCCee_v01_fields/ILD_FCCee_v01_fields.xml"
+    ),
+    "ILD_FCCee_v01_fields_noMask": DetectorConfig(
+        ddsim4FCC,
+        ild4FCCDir / "ILD_FCCee_v01_fields_noMask/ILD_FCCee_v01_fields_noMask.xml",
     ),
     "ILD_FCCee_v02": DetectorConfig(
-        ddsim4FCCee, ild4FCCDir / "ILD_FCCee_v02/ILD_FCCee_v02.xml"
+        ddsim4FCC, ild4FCCDir / "ILD_FCCee_v02/ILD_FCCee_v02.xml"
     ),
-    "ILD_FCCee_v01_fields": DetectorConfig(ddsim4FCCee, ild4FCCDir),
-    "ILD_FCCee_v01_fields_noMask": DetectorConfig(ddsim4FCCee, ild4FCCDir),
+    "ILD_l5_v02": DetectorConfig(ddsim4ILC, ild4ILCDir / "ILD_l5_v02.xml"),
+    "ILD_l5_v03": DetectorConfig(ddsim4ILC, ild4ILCDir / "ILD_l5_v03.xml"),
+    "ILD_l5_v05": DetectorConfig(ddsim4ILC, ild4ILCDir / "ILD_l5_v05.xml"),
 }
 
 beamStrahlungDataPaths = {
@@ -53,10 +64,29 @@ beamStrahlungDataPaths = {
 }
 
 
-def replaceBXNumberInString(bsTypeName, bsPath, bxN):
+def replaceBXNumberInString(bsTypeName: str, bsPath: Path, bxN: int) -> str:
     if bsTypeName == "ILC250":
         return str(bsPath).replace("#N", str(bxN).zfill(4))
     return str(bsPath).replace("#N", str(bxN))
+
+
+def checkMaxBXNumberExceeded(bsTypeName: str, bunchCrossing: int) -> bool:
+    """
+    check whether maximum number of bunch crossings per beam strahlung type is exceeded
+    """
+    if bsTypeName in {"FCC240", "FCC91"} and bunchCrossing > 100:
+        print(
+            f"\nThere are only 100 bunch crossing for {bsTypeName} available",
+            end="\n\n",
+        )
+        return True
+    if bsTypeName == "ILC250" and bunchCrossing > 1312:
+        print(
+            f"\nThere are only 1312 bunch crossing for {bsTypeName} available",
+            end="\n\n",
+        )
+        return True
+    return False
 
 
 def main():
@@ -78,6 +108,9 @@ def main():
 
             for bunchCrossing in range(1, bunchCrossingEnd + 1):
 
+                if checkMaxBXNumberExceeded(bsTypeName, bunchCrossing):
+                    break
+
                 bsPathWithBXNumber = replaceBXNumberInString(
                     bsTypeName, bsPath, bunchCrossing
                 )
@@ -86,12 +119,10 @@ def main():
                 for detModName, detModConfigs in detectorConfigs.items():
                     if detModName in detMods2Ana:
 
-                        ddsimFile = detModConfigs.ddsimFile
-
-                        # Construct the compactFile Name
+                        # Construct the compactFile name
                         compactFile = k4geoDir / detModConfigs.relativeCompactFilePath
 
-                        # Construct the output file name
+                        # Construct the output file names
                         outName = (
                             outDir
                             / f"{detModName}-{bsTypeName}-bX_{str(bunchCrossing).zfill(4)}-nEvts_{nEvents}"
@@ -100,10 +131,10 @@ def main():
                         outputLogFileName = outName.with_suffix(".log")
 
                         # Construct the command
-                        command = f"ddsim --steeringFile {ddsimFile} --compactFile {compactFile} --inputFile {bsPathWithBXNumber} --outputFile {outputFileName} --numberOfEvents {nEvents} --guineapig.particlesPerEvent {guineaPigPartPerE} > {outputLogFileName} 2>&1"
+                        command = f"ddsim --steeringFile {detModConfigs.ddsimFile} --compactFile {compactFile} --inputFile {bsPathWithBXNumber} --outputFile {outputFileName} --numberOfEvents {nEvents} --guineapig.particlesPerEvent {guineaPigPartPerE} > {outputLogFileName} 2>&1"
                         bsub_command = f'bsub -q l "{command}"'
 
-                        # Execute or print the command depending on execute_bsub
+                        # Execute or print the command depending on executeBsub
                         if executeBsub:
                             subprocess.run(bsub_command, shell=True)
                         else:
