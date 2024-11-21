824e670624a92994169b4d13a77ad8d18670b9f0
Victor Schwan <victor.schwan@cern.ch>
2024-11-21 19:03:51 +0100

    refac main function

--- simall.py
+++ simall.py
@@ -26,7 +26,7 @@ acceleratorConfigs2Ana = {"FCC091", "FCC240"}
 
 isExecutedOnDESYNAF = "desy.de" in Path.home().parts
 
-# define dirs to detector models
+# define paths for later use
 codeDir = Path(getenv("myCodeDir"))
 beamStrahlungCodeDir = codeDir / "beamStrahlung"
 k4geoDir = codeDir / "k4geo"
@@ -47,10 +47,14 @@ setupScriptPath = "/cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh"
 det_mod_configs_dict = get_paths_and_detector_configs(beamStrahlungCodeDir)
 
 
-def replaceBXNumberInString(bsTypeName: str, bsPath: Path, bxN: int) -> str:
+def replaceBXNumberInString(bsTypeName: str, bxN: int) -> str:
     if bsTypeName == "ILC250":
-        return str(bsPath).replace("#N", str(bxN).zfill(4))
-    return str(bsPath).replace("#N", str(bxN))
+        return str(get_path_for_current_machine(bs_data_paths[bsTypeName])).replace(
+            "#N", str(bxN).zfill(4)
+        )
+    return str(get_path_for_current_machine(bs_data_paths[bsTypeName])).replace(
+        "#N", str(bxN)
+    )
 
 
 def checkMaxBXNumberExceeded(bsTypeName: str, bunchCrossing: int) -> bool:
@@ -85,43 +89,39 @@ def main():
     outDir.mkdir(parents=True, exist_ok=True)
 
     # Iterate over the beam strahlung scenarios
-    for bs_scenario_name, bs_path_dict in bs_data_paths.items():
-        if bs_scenario_name in acceleratorConfigs2Ana:
-            for bunchCrossing in range(1, bunchCrossingEnd + 1):
-                if checkMaxBXNumberExceeded(bs_scenario_name, bunchCrossing):
-                    break
+    for bs_scenario_name in acceleratorConfigs2Ana:
 
-                bsPathWithBXNumber = replaceBXNumberInString(
-                    bs_scenario_name,
-                    get_path_for_current_machine(bs_path_dict),
-                    bunchCrossing,
-                )
+        for bunchCrossing in range(1, bunchCrossingEnd + 1):
+            if checkMaxBXNumberExceeded(bs_scenario_name, bunchCrossing):
+                break
 
-                # Iterate over the detector models
-                for detModName, detModConfigs in det_mod_configs_dict.items():
-                    if detModName in detMods2Ana:
-                        # Construct the compactFile name
-                        compactFile = (
-                            k4geoDir / detModConfigs.relative_compact_file_path
-                        )
+            bsPathWithBXNumber = replaceBXNumberInString(
+                bs_scenario_name, bunchCrossing
+            )
 
-                        # Construct the output file names
-                        outName = (
-                            outDir
-                            / f"{detModName}-{bs_scenario_name}-bX_{str(bunchCrossing).zfill(4)}-nEvts_{nEvents}"
-                        )
-                        outputFileName = outName.with_suffix(".edm4hep.root")
-                        outputLogFileName = outName.with_suffix(".log")
+            # Iterate over the detector models
+            for detModName, detModConfigs in det_mod_configs_dict.items():
+                if detModName in detMods2Ana:
+                    # Construct the compactFile name
+                    compactFile = k4geoDir / detModConfigs.relative_compact_file_path
 
-                        # Construct the command
-                        command = f"ddsim --steeringFile {detModConfigs.ddsim_file} --compactFile {compactFile} --inputFile {bsPathWithBXNumber} --outputFile {outputFileName} --numberOfEvents {nEvents} --guineapig.particlesPerEvent {guineaPigPartPerE} > {outputLogFileName} 2>&1"
-                        bsub_command = f'bsub -q l "{command}"'
+                    # Construct the output file names
+                    outName = (
+                        outDir
+                        / f"{detModName}-{bs_scenario_name}-bX_{str(bunchCrossing).zfill(4)}-nEvts_{nEvents}"
+                    )
+                    outputFileName = outName.with_suffix(".edm4hep.root")
+                    outputLogFileName = outName.with_suffix(".log")
 
-                        # Execute or print the command depending on executeBsub
-                        if executeBsub:
-                            subprocess.run(bsub_command, shell=True, check=True)
-                        else:
-                            print(bsub_command, end="\n\n")
+                    # Construct the command
+                    command = f"ddsim --steeringFile {detModConfigs.ddsim_file} --compactFile {compactFile} --inputFile {bsPathWithBXNumber} --outputFile {outputFileName} --numberOfEvents {nEvents} --guineapig.particlesPerEvent {guineaPigPartPerE} > {outputLogFileName} 2>&1"
+                    bsub_command = f'bsub -q l "{command}"'
+
+                    # Execute or print the command depending on executeBsub
+                    if executeBsub:
+                        subprocess.run(bsub_command, shell=True, check=True)
+                    else:
+                        print(bsub_command, end="\n\n")
 
 
 if __name__ == "__main__":
