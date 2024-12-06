c6958d6b77717bb4971ed0879b6471a32dae0ce0
Victor Schwan <victor.schwan@cern.ch>
2024-12-06 11:09:58 +0100

    Revert "more resources for ILC jobs"
    This reverts commit 5f972bda594f1a2481dd47032313437529bab1e2.

--- simall.py
+++ simall.py
@@ -176,7 +176,6 @@ def main():
 
                     # Decide whether to use Condor or bsub
                     batch_system = "condor" if isExecutedOnDESYNAF else "bsub"
-                    more_rsrcs = "ILC" in bs_scenario_name
 
                     # Submit the job using the appropriate batch system
                     submit_job(
@@ -186,7 +185,6 @@ def main():
                         args.submit_jobs,
                         beamStrahlungCodeDir,
                         executable,
-                        more_resources=more_rsrcs,
                     )
 
 
