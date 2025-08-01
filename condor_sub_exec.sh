a86d9a390a6b32d6d449706c1708fec755f81197
Victor Schwan <victor.schwan@cern.ch>
2025-08-01 14:32:30 +0200

    condor: print ddsim command and $K4GEO in job log

--- condor_sub_exec.sh
+++ condor_sub_exec.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 source /afs/desy.de/user/s/schwanvl/small_code/setup/setup6.sh
+n=90
+printf '*%.0s' $(seq 1 $n); printf '\n'
+echo ddsim "$@"
+printf '*%.0s' $(seq 1 $n); printf '\n'
+echo ${K4GEO}
+printf '*%.0s' $(seq 1 $n); printf '\n'
 ddsim "$@"
