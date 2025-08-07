48f8596b5c00bcabb2ae61baa4ee92513390b302
Bruno Francis Keyworth <keyworth@naf-ilc22.desy.de>
2025-08-07 16:13:54 +0200

    Created table of hits/bx/mm^2

--- condor_sub_exec.sh
+++ condor_sub_exec.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
-source /afs/desy.de/user/k/keyworth/promotion/code/beamStrahlung/env_setup.sh
-=======
 source ${codeDir}/beamStrahlung/env_setup.sh
->>>>>>> upstream/main
 n=90
 printf '*%.0s' $(seq 1 $n); printf '\n'
 echo ddsim "$@"
