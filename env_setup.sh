83346013746b1a7d9258561e5721f38916bc9a4f
Bruno Francis Keyworth <keyworth@naf-ilc-gpu01.desy.de>
2025-08-13 13:27:48 +0200

    Fixed bugs

--- env_setup.sh
+++ env_setup.sh
@@ -2,4 +2,5 @@ echo "set local k4geo"
 export k4geo_DIR="$k4gDir"
 export K4GEO="$k4geo_DIR"
 echo "source key4hep stack"
-source /cvmfs/sw.hsf.org/key4hep/releases/2025-01-28/setup.sh
+source /cvmfs/sw.hsf.org/key4hep/setup.sh -r 2025-01-28
+
